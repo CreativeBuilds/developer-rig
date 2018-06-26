d18af58c5f145fcfe1331e929ad1f1c9d379b6f1
CreativeBuilds <gaminggonecreative@gmail.com>
2018-06-26 18:10:24 -0400

    added allow origin all

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 var app = require('express')();
 var http = require('http').Server(app);
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 var fs = require("fs");
 
