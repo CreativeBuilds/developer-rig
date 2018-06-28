4cdc72947562758b6a9fbbcecad58a9bc18b4b09
CreativeBuilds <gaminggonecreative@gmail.com>
2018-06-28 10:10:41 -0400

    Updated allow credentials

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,7 @@ app.use(cors({origin:"https://localhost.rig.twitch.tv:8081"}))
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "https://localhost.rig.twitch.tv:8081");
+    res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
