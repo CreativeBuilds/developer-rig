1b550bed393b869699a3715c8010de20c2fd3989
CreativeBuilds <gaminggonecreative@gmail.com>
2018-06-28 09:50:38 -0400

    updated origin card so it's not *, so chrome wont yell

--- backend/index.js
+++ backend/index.js
@@ -61,7 +61,7 @@ connection.connect(function (err) {
         function run() {
             // Import all extra stuff
 
-            var io = require('socket.io')(https, {origins:'*'});
+            var io = require('socket.io')(https, {origins:'*.*'});
 
             const Boss = class Boss {
                 //Initiates the boss
